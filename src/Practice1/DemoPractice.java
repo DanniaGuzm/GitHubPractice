41987eed0b450b96cb097fe61c9c80127ba82093
DanniaG <DanniaG@DanniaG-PC.fios-router.home>
2021-12-22 19:21:22 -0500

    GirHub Practice with Ahmad.

---
+++ src/Practice1/DemoPractice.java
@@ -0,0 +1,18 @@
+package Practice1;
+
+public class DemoPractice {
+	
+	public static void main (String[] args) {
+		
+		int Number = 7;
+		
+		if (Number %2 ==0) {
+			
+			System.out.println("Even Number");
+			
+		}else if (Number %2 ==1) {
+			System.out.println("Odd Mumber");
+		}
+		}
+
+}
